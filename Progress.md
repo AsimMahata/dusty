3ec3d32c2add250cf1d1f9ce9901fd8db313d0d3
AsimMahata <asimmahata02@gmail.com>
2026-06-16 22:59:00 +0530

    more adhoc

---
+++ Progress.md
@@ -0,0 +1,7 @@
+# 16th June
+
+## Plan
+
+- make different section which will handle different things
+- for now video , image , audio these 3 things
+- So We will scan and according to category will send it to handlers
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,93 @@
-use mime_guess::{self};
+use mime_guess::{self, mime};
 use std::{
     collections::HashMap,
-    fs::{self, DirEntry},
+    fs::{self},
+    io,
+    path::PathBuf,
 };
 
-fn read_all_video_files(path: String) -> HashMap<String, Vec<DirEntry>> {
-    let mut results: HashMap<String, Vec<DirEntry>> = HashMap::new();
-    println!("this is the path: {:#?}", path);
+pub type Data = HashMap<String, Vec<PathBuf>>;
 
-    let entries = fs::read_dir(&path).expect(&format!("failed to read directory {}", path));
+pub struct Files {
+    videos: Data,
+    audios: Data,
+    images: Data,
+    others: Data,
+}
+
+impl Files {
+    pub fn new() -> Self {
+        Files {
+            videos: HashMap::new(),
+            audios: HashMap::new(),
+            images: HashMap::new(),
+            others: HashMap::new(),
+        }
+    }
+}
+
+fn video_handler(files: &mut Files, path: PathBuf) {
+    if let Some(ext) = path.extension().expect("extension not found").to_str() {
+        files.videos.entry(ext.to_string()).or_default().push(path);
+    }
+}
+
+fn audio_handler(files: &mut Files, path: PathBuf) {
+    if let Some(ext) = path.extension().expect("extension not found").to_str() {
+        files.audios.entry(ext.to_string()).or_default().push(path);
+    }
+}
+
+fn image_handler(files: &mut Files, path: PathBuf) {
+    if let Some(ext) = path.extension().expect("extension not found").to_str() {
+        files.images.entry(ext.to_string()).or_default().push(path);
+    }
+}
+
+fn default_handler(files: &mut Files, path: PathBuf) {
+    if let Some(ext) = path.extension().expect("extension not found").to_str() {
+        files.others.entry(ext.to_string()).or_default().push(path);
+    }
+}
+
+fn read_all_files(files: &mut Files, path: PathBuf) {
+    println!("this is the path: {:#?}", path);
+    let entries = fs::read_dir(&path).expect(&format!("failed to read directory {:?}", path));
 
     for entry in entries {
-        let dir_entry = entry.expect("something wrong with this file");
+        let file_path = entry.expect("something wrong with this file").path();
+        let guess = mime_guess::from_path(&file_path);
 
-        let guess = mime_guess::from_path(dir_entry.path());
         match guess.first() {
-            Some(m) => results
-                .entry(m.type_().to_string())
-                .or_default()
-                .push(dir_entry),
+            Some(m) => match m.type_() {
+                mime::VIDEO => video_handler(files, file_path),
+                mime::AUDIO => audio_handler(files, file_path),
+                mime::IMAGE => image_handler(files, file_path),
+                _ => default_handler(files, file_path),
+            },
             _ => {}
         }
     }
+}
 
-    return results;
+fn what_to_print(files: &Files) {
+    let mut buf = String::new();
+    println!("1: Video \n2: Audio\n3: Images \n4: Others \n-1: Exit");
+    io::stdin().read_line(&mut buf).expect("failed to read");
+    let num: i32 = buf.trim().parse().expect("failed to parse");
+    match num {
+        1 => println!("{:#?}", files.videos),
+        2 => println!("{:#?}", files.audios),
+        3 => println!("{:#?}", files.images),
+        4 => println!("{:#?}", files.others),
+        -1 => println!("Exit"),
+        _ => println!("Invalid choice"),
+    }
 }
 
 fn main() {
-    let path: String = String::from("C:\\Users\\asim\\Downloads\\Telegram Desktop");
-
-    let results: HashMap<String, Vec<DirEntry>> = read_all_video_files(path);
-
-    // Standard debug print
-    println!("{:#?}", results);
+    let path: PathBuf = PathBuf::from("C:\\Users\\asim\\Downloads");
+    let mut files = Files::new();
+    read_all_files(&mut files, path);
+    what_to_print(&files);
 }
